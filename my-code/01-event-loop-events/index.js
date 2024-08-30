1ef908ba446528d610cb357395f63098af0cb0d9
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-08-30 17:59:21 +0300

    lesson 36

--- my-code/01-event-loop-events/index.js
+++ my-code/01-event-loop-events/index.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
+const dns = require('dns');
 
 const timestamp = () => performance.now().toFixed(2) + ' ms';
 
 console.log('Start');
 
-setTimeout(() => console.log('Timeout 1 sec', timestamp()), 1000);
-setTimeout(() => console.log('Timeout 0 sec'), 0);
-
-setImmediate(() => console.log('Immediate 1', timestamp()));
-
+//Close events
 fs.writeFile('./my-code/01-event-loop-events/test.txt', 'Hello Node.js', () => console.log('File written'));
 
+//Promises
 Promise.resolve().then(() => console.log('Promise 1', timestamp()));
 
-process.nextTick(() => console.log('Next tick', timestamp()));
+//Next tick
+process.nextTick(() => console.log('Next tick 1', timestamp()));
+
+//setImmediate (Check )
+setImmediate(() => console.log('Immediate 1', timestamp()));
+
+//I/O events
+dns.lookup('localhost', (err, address, family) => {
+    console.log('DNS 4 localhost', address, timestamp()); 
+    Promise.resolve().then(() => console.log('Promise 2', timestamp()));
+    process.nextTick(() => console.log('Next tick 3', timestamp()));
+});
+
+//Timeouts
+setTimeout(() => console.log('Timeout 1', timestamp()), 1);
+setTimeout(() => {
+    process.nextTick(() => console.log('Next tick 2', timestamp())); 
+    console.log('Timeout 2', timestamp());
+}, 1);
 
 console.log('End');

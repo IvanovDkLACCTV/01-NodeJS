e918e77fabf3db5b3027decfa7ea9225a675b540
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-10-25 17:59:01 +0300

    started lesson 86

---
+++ my-code/07-path/index.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+const filePath = 'C:/dev/01-NodeJS/my-code/06-events/index.js'
+const textFilePath = 'C:/dev/01-NodeJS/my-code/06-events/second.txt'
+const relativePath = './06-events/fs-events.js'

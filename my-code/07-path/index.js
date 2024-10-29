eee644ace1ceb3a4fff155b18e5fceb5185512a9
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-10-29 17:58:00 +0300

    finished chapter 12

--- my-code/07-path/index.js
+++ my-code/07-path/index.js
@@ -1,5 +1,20 @@
 const path = require('path')
 
+const log = console.log
+
 const filePath = 'C:/dev/01-NodeJS/my-code/06-events/index.js'
-const textFilePath = 'C:/dev/01-NodeJS/my-code/06-events/second.txt'
+const textFilePath = './dev/01-NodeJS/my-code/06-events/second.txt'
 const relativePath = './06-events/fs-events.js'
+const derectoryPath = './my-code/06-events'
+
+log(path.basename(derectoryPath))
+log(path.isAbsolute(relativePath))
+log(path.dirname(textFilePath ))
+log(path.resolve(filePath))
+
+log(path.extname(textFilePath))
+log(path.parse(filePath))
+
+const parsedPath = path.parse(filePath)
+log(path.resolve(filePath))
+log(path.join(parsedPath.dir, `parsed-${parsedPath.name}.js`))

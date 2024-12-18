91059aad9e108debfb42178220fd9e5a0f050f3b
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-12-18 17:59:30 +0300

    lesson 109

--- my-code/09-streams/stdin-pipe-stdout.mjs
+++ my-code/09-streams/stdin-pipe-stdout.mjs
@@ -1,12 +1,35 @@
-import stream from "stream"
+import { Transform } from "stream"
 import fs from "fs"
 
 const log = console.log
-const filePath = "./files/stdin-dump.txt" //pipe to file
+const upperCaseStream = new Transform({
+  transform: function (chunk, encoding, cb) {
+    // const upperCased = chunk
+    const upperCased = chunk.toString().toUpperCase()
+    //log(upperCased)
+    cb(null, upperCased)
+  },
+})
 
-const writeStream = fs.createWriteStream(filePath)
+const reverseStream = new Transform({
+  transform(chunk, encoding, cb) {
+    const arrayOfChars = chunk.toString().split("")
+    const theLastChar = arrayOfChars.pop()
+    const reversedString = arrayOfChars.reverse().concat(theLastChar).join("")
+    //log(reversedString)
+    cb(null, reversedString)
+  },
+})
 
-process.stdin.pipe(writeStream)
+process.stdin.pipe(upperCaseStream).pipe(reverseStream).pipe(process.stdout)
+// const filePath = "./files/stdin-dump.txt" //pipe to file
 
-//pipe to stdout
-process.stdin.pipe(process.stdout)
+// const writeStream = fs.createWriteStream(filePath)
+
+// process.stdin.pipe(writeStream)
+
+// //pipe to stdout
+// process.stdin.pipe(process.stdout)
+// process.stdin.pipe(process.stderr)
+
+//node stdin-pipe-stdout.mjs

0baff3084aba029133f65b55795d543ba87e70af
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-01-23 11:25:44 +0300

    lesson 147

--- my-code/12-express/03-routes/app.js
+++ my-code/12-express/03-routes/app.js
@@ -18,9 +18,15 @@ const postCommentsHandler = (req, res) => {
   res.send("Post comments route")
 }
 
+const getCommentHandler = (req, res) => {
+  const { commentId } = req.params
+  res.send(`Get comment with id ${commentId} route`)
+}
+
 app.get("/", getRootandler)
 app.get("/comments", getCommentsHandler)
 app.post("/comments", postCommentsHandler)
+app.get("/comments/:commentId", getCommentHandler)
 
 //start server
 app.listen(PORT, () => log(`Server was started on port ${PORT}`))

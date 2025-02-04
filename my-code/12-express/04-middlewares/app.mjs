14cf08f68c42c059c9dd04df517baf2d4fc1d1b1
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-02-04 17:59:06 +0300

    lesson 159

--- my-code/12-express/04-middlewares/app.mjs
+++ my-code/12-express/04-middlewares/app.mjs
@@ -1,19 +1,20 @@
 import express from "express"
+import morgan from "morgan"
 
 const PORT = 5006
 const log = console.log
 
 const app = express()
 
-const logger = (req, res, next) => {
-  log(req.method, req.path)
-  next()
-}
+// const logger = (req, res, next) => {
+//   log(req.method, req.path)
+//   next()
+// }
 
-//app.use(logger)
+// app.use(logger)
 
-//app.get("/", logger, () => {res.send()})
+app.use(morgan("combined"))
 
-app.use(logger, (req, res) => res.send("This is Express.JS server"))
+app.use((req, res) => res.send("This is Express.JS server"))
 
 app.listen(PORT, () => log(`Server is listenning a port ${PORT}`))

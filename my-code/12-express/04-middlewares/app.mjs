1e7fafb2963778361aaa1377b8e7c25efed62422
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-02-05 10:08:15 +0300

    lesson 161

--- my-code/12-express/04-middlewares/app.mjs
+++ my-code/12-express/04-middlewares/app.mjs
@@ -8,13 +8,21 @@ const app = express()
 
 app.use(morgan("tiny"))
 
-app.use((req, res, next) => {
-  let data = ""
-  req.on("data", (chunk) => (data += chunk))
-  req.on("end", () => log(JSON.parse(data)))
-  next()
-})
+app.use(express.json())
+// =
+// app.use((req, res, next) => {
+//   let data = ""
+//   req.on("data", (chunk) => (data += chunk))
+//   req.on("end", () => {
+//     const parsedJSON = JSON.parse(data)
+//     req.body = parsedJSON
+//     next()
+//   })
+// })
 
-app.use((req, res) => res.send("This is Express.JS server"))
+app.use((req, res) => {
+  log(req.body)
+  res.send("This is Express.JS server")
+})
 
 app.listen(PORT, () => log(`Server is listenning a port ${PORT}`))

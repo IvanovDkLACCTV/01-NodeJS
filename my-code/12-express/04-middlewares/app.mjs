72ae37708f00d44cb11702655d6876380c5777fd
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-02-05 10:33:11 +0300

    lesson 162

--- my-code/12-express/04-middlewares/app.mjs
+++ my-code/12-express/04-middlewares/app.mjs
@@ -1,5 +1,6 @@
 import express from "express"
 import morgan from "morgan"
+import qs from "querystring"
 
 const PORT = 5006
 const log = console.log
@@ -9,16 +10,20 @@ const app = express()
 app.use(morgan("tiny"))
 
 app.use(express.json())
-// =
-// app.use((req, res, next) => {
-//   let data = ""
-//   req.on("data", (chunk) => (data += chunk))
-//   req.on("end", () => {
-//     const parsedJSON = JSON.parse(data)
-//     req.body = parsedJSON
-//     next()
-//   })
-// })
+
+app.use((req, res, next) => {
+  if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
+    let data = ""
+    req.on("data", (chunk) => (data += chunk.toString()))
+    req.on("end", () => {
+      const parsedFormData = qs.parse(data)
+      req.body = parsedFormData
+      next()
+    })
+  } else {
+    next()
+  }
+})
 
 app.use((req, res) => {
   log(req.body)

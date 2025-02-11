fbee5b7d1f77b79c51008480a5c1c172e2bd4c60
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-02-11 17:29:27 +0300

    lesson 168

--- my-code/12-express/05-frontend-and-backend/backend/app.mjs
+++ my-code/12-express/05-frontend-and-backend/backend/app.mjs
@@ -1,14 +1,19 @@
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
 
 const PORT = 5006
 const log = console.log
 
 const app = express()
 
+//Middleware
 app.use(morgan("tiny"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
+//
 
 app.use((req, res) => {
   const personData = {
